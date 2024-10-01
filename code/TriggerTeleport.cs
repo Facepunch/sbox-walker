99e29724168c43b2994b1f23d2b833d29d0c281f
Garry Newman <garrynewman@users.noreply.github.com>
2024-10-01 15:53:35 +0100

    TriggerTeleport

---
+++ code/TriggerTeleport.cs
@@ -0,0 +1,35 @@
+public sealed class TriggerTeleport : Component, Component.ITriggerListener
+{
+	[Property] public GameObject Target { get; set; }
+	[Property] public Action<GameObject> OnTeleported { get; set; }
+
+	protected override void DrawGizmos()
+	{
+		Gizmo.Draw.Arrow( 0, Transform.World.PointToLocal( Target.Transform.Position ) );
+	}
+
+	void ITriggerListener.OnTriggerEnter( Collider other )
+	{
+		var go = other.GameObject;
+
+		if ( !IsValidTarget( ref go ) ) return;
+
+		go.Transform.Position = Target.Transform.Position;
+		go.Transform.ClearInterpolation();
+
+		DoTeleportedEvent( go );
+	}
+
+	bool IsValidTarget( ref GameObject go )
+	{
+		go = go.Root;
+		if ( go.IsProxy ) return false;
+		return true;
+	}
+
+	[Broadcast]
+	void DoTeleportedEvent( GameObject obj )
+	{
+		OnTeleported?.Invoke( obj );
+	}
+}
